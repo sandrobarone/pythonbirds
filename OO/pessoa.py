812b968269985c1943fcbf3625f26c05795bc15f
sandrobarone <sandrobarone@gmail.com>
2020-05-13 21:05:52 -0300

    Criada a classe Pessoa

---
+++ OO/pessoa.py
@@ -0,0 +1,14 @@
+"""
+Classe:
+    Classes devem comecar com a Letra Maiuscula (camel case : ExemploPessoa)
+    É a base da OO.
+    É a fôrma (fôrma de gelo) que define como nossos objetos se comportam. As vezes é utilizado
+como sinonimo de "tipo".
+
+Funcao:
+    Funcoes devem comecar com a Letra Minuscula (snake case : exemplo_pessoa)
+"""
+
+
+class Pessoa:
+    pass
